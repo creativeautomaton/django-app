0aa9a2b7172d3332a6e4538687fec8b859a6a6d8
zachary kraft <creativeautomaton@gmail.com>
2019-12-05 19:44:20 -0600

    removed examples projects

--- docker-django-react/backend/core/local_settings.sample.py
+++
@@ -1,9 +0,0 @@
-from .settings import *
-
-NODE_ENV = os.getenv('NODE_ENV', default="development")
-
-if DEBUG:
-    MIDDLEWARE.append('api.middleware.dev_cors_middleware')
-
-if not DEBUG and NODE_ENV == "production":
-    STATICFILES_DIRS.append(os.path.join(REACT_APP_DIR, 'build', 'static'))
